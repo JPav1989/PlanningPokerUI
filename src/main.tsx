2056487d0e2c55035436e71b25fb6ce4a4378e02
John Pavier <51865380+JPav1989@users.noreply.github.com>
2025-04-05 20:56:50 +0100

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client'; // Use createRoot for React 18+
 import App from './App';
 
-const root = document.getElementById('root');
-ReactDOM.render(
+const rootElement = document.getElementById('root');
+
+// For React 18 and later:
+const root = ReactDOM.createRoot(rootElement);
+root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  root
+  </React.StrictMode>
 );
